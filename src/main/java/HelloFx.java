8b2b915501dd5d87dbf821547ff124e558252a24
Edgar <edgargonzalez.info@gmail.com>
2017-07-28 13:41:08 +0200

    firsLesson

--- src/main/java/HelloFx.java
+++ src/main/java/HelloFx.java
@@ -1,17 +1,46 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 
-public class HelloFx extends Application{
+public class HelloFx extends Application {
 
+
+    public static void main(String[] args) {
+        Application.launch(args);
+
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("HelloFx");
+
+        stage.setTitle("Do-It");
+        Group group = new Group();
+        Button b1 = new Button("Fist Button");
+        Button b2 = new Button("Second Button");
+        group.getChildren().addAll(b1, b2);
+        b1.setLayoutX(50);
+        b1.setLayoutX(150);
+
+        Scene scene = new Scene(group,300,300);
+        stage.setScene(scene);
+        stage.setAlwaysOnTop(true);//con esta linea lo que hacemos es mantener simepre la ventana visible
+        stage.setResizable(false); // con esta line impedimos que la ventana se dimensione
         stage.show();
 
     }
 
+    @Override
+    public void init() throws Exception {
+        System.out.println("Este es el metodo init");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Este es el metodo stop");
+    }
 
 
 }
