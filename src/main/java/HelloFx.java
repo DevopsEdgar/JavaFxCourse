caf0fa3d2b99f417188fb11ffc95b41915389c12
Edgar <edgargonzalez.info@gmail.com>
2017-07-28 12:56:11 +0200

    Proyecto creado

---
+++ src/main/java/HelloFx.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class HelloFx extends Application{
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("HelloFx");
+        stage.show();
+
+    }
+
+
+
+}
